32374f9c28b5be2eac36eea3dd0d361df6ce0c30
rfeldman <rfeldman@leavitt.cm.cluster>
2019-03-09 13:51:58 -0500

    cleaned up main

--- file_manager/main.py
+++ file_manager/main.py
@@ -3,11 +3,7 @@ from read_parameters import Parameters
 import time
 
 def readDataFeed():
-    print('readDataFeed function started')
-    runs = []
-    last = len(runs)-1
     spreadsheetParameters = Parameters()
-    runs.append(spreadsheetParameters)
-    print(runs[last].__dict__)
+    print(spreadsheetParameters.__dict__)
     #make folders and param files
 readDataFeed()
