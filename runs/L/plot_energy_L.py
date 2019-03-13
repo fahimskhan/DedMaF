1b7839e6ca62d154a53543da38eb6003f5c959d6
bubbadaboss77 <rfeldman@bates.edu>
2019-03-13 19:17:59 -0400

    ok

--- runs/L/plot_energy_L.py
+++ runs/L/plot_energy_L.py
@@ -4,30 +4,39 @@ import h5py
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+from glob import glob
+
 basedir = Path.cwd()
-dfile = basedir.joinpath('scratch', 'kturb_run_L', 'timeseries', 'timeseries_s1', 'timeseries_s1_p0.h5')
-print(dfile)
-data = h5py.File(str(dfile), "r")
+datadir = basedir.joinpath('scratch', 'kturb_run_L', 'timeseries', 'timeseries_s1').glob('**/*')
+files = [str(x) for x in datadir if x.is_file()]
+print(files)
+print('--------------------')
+# dfile = basedir.joinpath('scratch', 'kturb_run_L', 'timeseries', 'timeseries_s1', 'timeseries_s1_p0.h5')
+# print(dfile)
 
-t = data['scales/sim_time'][:]
-KE = data['tasks/Ekin'][:,0,0]
-#sigma = data['tasks/Σ'][:,0,0]
-#sigma = data['tasks/Sigma'][:,0,0]
-plt.subplot(121)
-plt.plot(t, KE)
-plt.xlabel("time")
-plt.ylabel("Kinetic energy")
-plt.subplot(122)
-plt.plot(t, KE, 'x-')
-plt.xlim(0,500)
-plt.xlabel("time")
-plt.ylabel("Kinetic energy")
 
-# plt.plot(t, sigma)
-# plt.xlabel("time")
-# plt.ylabel(r"$\Sigma$")
 plot_dir = Path.cwd().joinpath('plots')
-print(plot_dir)
-plot_dir.mkdir(exist_ok=True, parents=True) 
-plt.savefig('plots/energy.png')
+plot_dir.mkdir(exist_ok=True, parents=True)
 
+def make_plot(filename):
+    data = h5py.File(str(filename), "r")
+    t = data['scales/sim_time'][:]
+    KE = data['tasks/Ekin'][:,0,0]
+    #sigma = data['tasks/Σ'][:,0,0]
+    #sigma = data['tasks/Sigma'][:,0,0]
+    plt.subplot(121)
+    plt.plot(t, KE)
+    plt.xlabel("time")
+    plt.ylabel("Kinetic energy")
+    plt.subplot(122)
+    plt.plot(t, KE, 'x-')
+    plt.xlim(0,500)
+    plt.xlabel("time")
+    plt.ylabel("Kinetic energy")
+    # plt.plot(t, sigma)
+    # plt.xlabel("time")
+    # plt.ylabel(r"$\Sigma$")
+    plt.savefig('plots/energy_{}.png'.format(i))
+
+for i in range(0, len(files)):
+    make_plot(i)
